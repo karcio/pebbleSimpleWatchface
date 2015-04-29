73ff13ce179773336a9eef47cdd65393fcb5accd
Karol Czarnecki <kczarnecki@hotmail.com>
2015-04-29 13:07:55 +0100

    added date

--- src/main.c
+++ src/main.c
@@ -4,20 +4,25 @@ static BitmapLayer *s_background_layer;
 static GBitmap *s_background_bitmap;
 static Window *s_main_window;
 static TextLayer * s_time_layer;
+static TextLayer * s_date_layer;
 
 static void update_time() {
   time_t temp = time(NULL); 
   struct tm *tick_time = localtime(&temp);
 
-  static char buffer[] = "00:00";
-
+  static char timeBuffer[6] ;
+  static char dateBuffer[12];
+  
   if(clock_is_24h_style() == true) {
-    strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
+    strftime(timeBuffer, sizeof(timeBuffer), "%H:%M", tick_time);
   } else {
-    strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
+    strftime(timeBuffer, sizeof(timeBuffer), "%I:%M", tick_time);
   }
   
-  text_layer_set_text(s_time_layer, buffer);
+  text_layer_set_text(s_time_layer, timeBuffer);
+  
+  strftime(dateBuffer, sizeof(dateBuffer), "%e %b %Y", tick_time); 
+  text_layer_set_text(s_date_layer, dateBuffer);
 }
 
 static void main_window_load(Window *window) {
@@ -31,12 +36,19 @@ static void main_window_load(Window *window) {
   text_layer_set_text_color(s_time_layer, GColorWhite);
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_LIGHT));
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
-  
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+  
+  s_date_layer = text_layer_create(GRect(0, 95, 144, 50));
+  text_layer_set_background_color(s_date_layer, GColorClear);
+  text_layer_set_text_color(s_date_layer, GColorWhite);
+  text_layer_set_font(s_date_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
+  text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_date_layer));
 }
 
 static void main_window_unload(Window *window) {
   text_layer_destroy(s_time_layer);
+  text_layer_destroy(s_date_layer);
   gbitmap_destroy(s_background_bitmap);
   bitmap_layer_destroy(s_background_layer);
 }
@@ -55,6 +67,7 @@ static void init() {
   window_stack_push(s_main_window, true);
   update_time();
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  
 }
 
 static void deinit() {
